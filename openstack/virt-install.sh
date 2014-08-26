40c7cac5635b0c340b978ebb08f3b37b3bd6d9b0
David Brown <dmlb2000@gmail.com>
2014-08-25 20:20:20 -0700

    this is the right image size

--- openstack/virt-install.sh
+++ openstack/virt-install.sh
@@ -6,4 +6,4 @@ TMP_KS=$(mktemp)
 curl "$1" > $TMP_KS
 URL=$(grep ^url $TMP_KS  | head -n 1 | cut -d= -f2)
 ARCH=$(basename $URL)
-virt-install -n $(basename ${1/.ks/}) --memory 2048 --vcpus 1 -l $URL -x "ks=$1 serial text" --disk size=5 -w bridge=br0 --serial pty --virt-type kvm --arch $ARCH --noreboot
+virt-install -n $(basename ${1/.ks/}) --memory 2048 --vcpus 1 -l $URL -x "ks=$1 serial text" --disk size=8 -w bridge=br0 --serial pty --virt-type kvm --arch $ARCH --noreboot
