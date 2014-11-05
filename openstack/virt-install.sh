fd5ee5a96fa560aafc0abd93cc772e1f23afb053
root <root@bterm1.emsl.pnl.gov>
2014-11-04 16:33:05 -0800

    fix up this to work on el7 host

--- openstack/virt-install.sh
+++ openstack/virt-install.sh
@@ -3,7 +3,7 @@
 TMP_KS=$(mktemp)
 
 # install the kickstart by url in $1
-curl "$1" > $TMP_KS
+curl -L "$1" > $TMP_KS
 URL=$(grep ^url $TMP_KS  | head -n 1 | cut -d= -f2)
 ARCH=$(basename $URL)
-virt-install -n $(basename ${1/.ks/}) --memory 2048 --vcpus 1 -l $URL -x "ks=$1 serial text console=ttyS0" --disk size=8 -w network=default --console pty --virt-type kvm --arch $ARCH --noreboot
+virt-install -n $(basename ${1/.ks/}) --ram 2048 --vcpus 1 -l $URL -x "ks=$1 serial text console=ttyS0" --disk path=/var/lib/libvirt/images/$(basename ${1/.ks/}).qcow2,size=8,format=qcow2 -w network=default --console pty --virt-type kvm --arch $ARCH --noreboot
