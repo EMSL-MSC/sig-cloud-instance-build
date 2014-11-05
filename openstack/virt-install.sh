ca1b70f72899ff84dcc25be3e8de3370daa117ec
dmlb2000 <dmlb2000@gmail.com>
2014-11-04 19:10:15 -0800

    try and fix some stuf

--- openstack/virt-install.sh
+++ openstack/virt-install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -xe
 
+TOPDIR=$(dirname $0)
+
 # install the kickstart by url in $1
-URL=$(grep ^url $1  | head -n 1 | cut -d= -f2)
+URL=$(grep ^url $TOPDIR/$1  | head -n 1 | cut -d= -f2)
 ARCH=$(basename $URL)
 virt-install --connect=qemu:///system \
     -n ${1/.ks/} \
     --ram 2048 \
     --vcpus 1 \
     -l $URL \
-    --initrd-inject=$1 \
+    --initrd-inject=$TOPDIR/$1 \
     -x "ks=/$1 serial text console=ttyS0" \
     --disk path=/var/lib/libvirt/images/${1/.ks/}.qcow2,size=8,format=qcow2 \
     -w network=default \
