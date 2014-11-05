0008d2e4eea63996b85d4aa12dd381c8f40078c6
root <root@bterm1.emsl.pnl.gov>
2014-11-04 16:44:19 -0800

    add the no autoconsole bit

--- openstack/virt-install.sh
+++ openstack/virt-install.sh
@@ -6,4 +6,4 @@ TMP_KS=$(mktemp)
 curl -L "$1" > $TMP_KS
 URL=$(grep ^url $TMP_KS  | head -n 1 | cut -d= -f2)
 ARCH=$(basename $URL)
-virt-install --connect=qemu:///system -n $(basename ${1/.ks/}) --ram 2048 --vcpus 1 -l $URL -x "ks=$1 serial text console=ttyS0" --disk path=/var/lib/libvirt/images/$(basename ${1/.ks/}).qcow2,size=8,format=qcow2 -w network=default --console pty --virt-type kvm --arch $ARCH --noreboot
+virt-install --connect=qemu:///system -n $(basename ${1/.ks/}) --ram 2048 --vcpus 1 -l $URL -x "ks=$1 serial text console=ttyS0" --disk path=/var/lib/libvirt/images/$(basename ${1/.ks/}).qcow2,size=8,format=qcow2 -w network=default --console pty --virt-type kvm --arch $ARCH --noreboot --noautoconsole
