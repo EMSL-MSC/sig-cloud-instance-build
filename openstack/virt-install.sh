23526d1a3461238ccda506e31df7d41f740820f8
dmlb2000 <dmlb2000@gmail.com>
2014-11-04 19:15:39 -0800

    this is a better kickstart options if using initrd-inject

--- openstack/virt-install.sh
+++ openstack/virt-install.sh
@@ -11,7 +11,7 @@ virt-install --connect=qemu:///system \
     --vcpus 1 \
     -l $URL \
     --initrd-inject=$TOPDIR/$1 \
-    -x "ks=/$1 serial text console=ttyS0" \
+    -x "ks=file:/$1 serial text console=ttyS0" \
     --disk path=/var/lib/libvirt/images/${1/.ks/}.qcow2,size=8,format=qcow2 \
     -w network=default \
     --console pty \
